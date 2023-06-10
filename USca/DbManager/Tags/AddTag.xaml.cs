c571e2617264d40577ef010145044ede5ba74de6
magley <60552482+magley@users.noreply.github.com>
2023-06-10 23:51:35 +0200

    feat: Finish add tag gui window

--- USca/DbManager/Tags/AddTag.xaml.cs
+++ USca/DbManager/Tags/AddTag.xaml.cs
@@ -1,16 +1,47 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
 
 namespace USca_DbManager.Tags
 {
+    public enum TagMode
+    {
+        Input,
+        Output
+    }
+
+    public enum TagType
+    {
+        Digital,
+        Analog
+    }
+
     public partial class AddTag : Window, INotifyPropertyChanged
     {
+        public List<string> TagUnits { get; set; } = new();
+
         public string TagName { get; set; } = "";
+        public string TagDesc { get; set; } = "";
+        public TagMode TagMode { get; set; }
+        public TagType TagType { get; set; }
+        public int TagAddress { get; set; }
+        public double TagMin { get; set; } = 0;
+        public double TagMax { get; set; } = 10.0;
+        public string TagUnit { get; set; } = "";
+        public int TagScanTime { get; set; } = 1000;
 
         public AddTag()
         {
             InitializeComponent();
             TxtTagName.Focus();
+
+            TagUnits.Add("mm");
+            TagUnits.Add("cm");
+            TagUnits.Add("°C");
+            TagUnits.Add("litre");
+            TagUnits.Add("kg");
+            TagUnits.Add("g");
+            TagUnit = TagUnits[0];
         }
 
         private void BtnCancel_Click(object sender, RoutedEventArgs e)
---
+++ USca/DbManager/Util/EnumToBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+using System.Globalization;
+
+namespace USca_DbManager.Util
+{
+    public class EnumToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.Equals(false))
+            {
+                return DependencyProperty.UnsetValue;
+
+            }
+            return parameter;
+        }
+    }
+}
