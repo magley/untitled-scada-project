1abc6bf2b3728cb28b5cacb8ff2c43568957910f
magley <60552482+magley@users.noreply.github.com>
2023-07-01 21:36:27 +0200

    fix: Use ctx per request in user service

--- USca/USca-Server/Users/UserService.cs
+++ USca/USca-Server/Users/UserService.cs
@@ -4,25 +4,22 @@ namespace USca_Server.Users
 {
     public class UserService : IUserService
     {
-        private readonly ServerDbContext _context;
-        public UserService(ServerDbContext context)
-        {
-            _context = context;
-        }
-
         public User? Login(LoginDTO loginCredentials)
         {
-            var user = _context.Users.Where(u => u.Username == loginCredentials.Username).FirstOrDefault();
-            if (user == null)
+            using (var db = new ServerDbContext())
             {
+                var user = db.Users.Where(u => u.Username == loginCredentials.Username).FirstOrDefault();
+                if (user == null)
+                {
+                    return null;
+                }
+
+                if (user.Password == loginCredentials.Password)
+                {
+                    return user;
+                }
                 return null;
             }
-
-            if (user.Password == loginCredentials.Password)
-            {
-                return user;
-            }
-            return null;
         }
     }
 }
