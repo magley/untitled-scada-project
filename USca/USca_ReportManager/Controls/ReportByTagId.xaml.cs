30e0e19eed14d1a08f4b6c7f5181bbaecf7b37ff
magley <60552482+magley@users.noreply.github.com>
2023-07-07 15:40:00 +0200

    feat: Implement reports for tag by id

---
+++ USca/USca_ReportManager/Controls/ReportByTagId.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using USca_ReportManager.Util;
+
+namespace USca_ReportManager.Controls
+{
+    public partial class ReportByTagId : UserControl
+    {
+        public ObservableCollection<TagLogDTO> TagLogs { get; set; } = new();
+        private TagLogService _tagLogService = new();
+
+        public ReportByTagId()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string searchQuery = TxtTagID.Text;
+            if (!int.TryParse(searchQuery,  out int id))
+            {
+                MessageBox.Show("Please enter an integer!", "Failure", MessageBoxButton.OK);
+                return;
+            }
+
+            var li = await _tagLogService.GetByTag(id);
+
+            TagLogs.Clear();
+            foreach (var o in li)
+            {
+                TagLogs.Add(o);
+            }
+        }
+    }
+}
---
+++ USca/USca_ReportManager/Util/TagLogService.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace USca_ReportManager.Util
+{
+    public class TagLogService
+    {
+        private readonly string URL = "http://localhost:5274/api";
+
+        public async Task<List<TagLogDTO>> GetByTag(int tagID)
+        {
+            using var cli = new RestClient(new RestClientOptions(URL));
+            var req = new RestRequest($"tag/logs/all/{tagID}", Method.Get);
+            RestResponse response = await cli.ExecuteAsync(req);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var li = JsonSerializer.Deserialize<List<TagLogDTO>>(response.Content);
+                return li;
+            }
+            else
+            {
+                throw new Exception(response.StatusCode.ToString());
+            }
+        }
+    }
+}
